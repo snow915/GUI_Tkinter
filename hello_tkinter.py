e2725351180da7e2b84c327d78892bbf494f03e9
snow915 <cris_ganor@hotmail.com>
2020-07-13 12:40:33 -0500

    modify: now it have 2 buttons that change the message of hello

--- hello_tkinter.py
+++ hello_tkinter.py
@@ -1,6 +1,29 @@
 from tkinter import *
+from tkinter import ttk
 
 
-root = Tk()
-Label(root, text="Hello Tkinter").pack()
-root.mainloop()
+class Hello:
+
+    def __init__(self, root):
+
+        self.label = ttk.Label(root, text="Hello Tkinter!")
+        self.label.grid(row=0, column=0, columnspan=2)
+
+        ttk.Button(root, text="Spanish", command=self.spanish_hello).grid(row=1, column=0)
+        ttk.Button(root, text="German", command=self.german_hello).grid(row=1, column=1)
+
+    def spanish_hello(self):
+        self.label.config(text="Hola Tkinter!")
+
+    def german_hello(self):
+        self.label.config(text="Hallo Tkinter!")
+
+
+def main():
+
+    root = Tk()
+    app = Hello(root)
+    root.mainloop()
+
+
+if __name__ == "__main__": main()
