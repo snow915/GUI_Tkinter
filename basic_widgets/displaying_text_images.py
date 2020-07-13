c1006d06e62ee33bcbf51fe0545f8f82edf91cad
snow915 <cris_ganor@hotmail.com>
2020-07-13 15:04:20 -0500

    modify: set the text dynamically with buttons, it can be better

--- basic_widgets/displaying_text_images.py
+++ basic_widgets/displaying_text_images.py
@@ -2,23 +2,55 @@ from tkinter import *
 from tkinter import ttk
 
 
+class ImageText:
+    def __init__(self, root):
+        self.label = ttk.Label(root, text="This is a text", foreground="blue", font=("Inconsolata", 20, "bold"))
+        self.label.grid(row=0, column=0, columnspan=2)
+        self.logo = PhotoImage(file="/home/snow/Pictures/logoOficial.png")
+        self.label.img = self.logo
+        self.label.config(image=self.label.img)
+        self.label.config(compound="center")
+        self.show_buttons(root)
+
+    def show_buttons(self, root):
+        positions = {
+            "left": self.set_text_left,
+            "center": self.set_text_center,
+            "right": self.set_text_right,
+            "top": self.set_text_top,
+            "bottom": self.set_text_bottom
+        }
+
+        counter = 0
+        for key, function in positions.items():
+            ttk.Button(root,
+                       text=key.capitalize(),
+                       command=function)\
+                .grid(row=1, column=counter)
+            counter += 1
+
+    # What really moves is the image
+    def set_text_right(self):
+        self.label.config(compound="left")
+
+    def set_text_center(self):
+        self.label.config(compound="center")
+
+    def set_text_left(self):
+        self.label.config(compound="right")
+
+    def set_text_top(self):
+        self.label.config(compound="bottom")
+
+    def set_text_bottom(self):
+        self.label.config(compound="top")
+
+
+
 def main():
 
     root = Tk()
-    label = ttk.Label(root, text="Hello")
-    label.pack()
-    label.config(text="Hello, Tkinter! It's been a while since we last met. Great to see you again!")
-    label.config(wraplength=150)
-    label.config(justify=CENTER)
-    label.config(foreground="blue", background="yellow")
-    label.config(font=("Courier", 18, "bold"))
-
-    logo = PhotoImage(file="/home/snow/Pictures/logoOficial.png")
-    label.img = logo
-    label.config(image=label.img)
-    label.config(compound="text")  # This will set the text instead of image
-    label.config(compound="center")  # This will set the text in front of image
-    label.config(compound="left")  # This will put the image to the left
+    app = ImageText(root)
     root.mainloop()
 
 
